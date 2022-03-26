5808eec1f73892891cef8fc5a273c61d182a32ec
whlee0525 <whlee0525@gmail.com>
2022-03-26 12:49:25 +0900

    update:meta to _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,21 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
+
 import Layout from "../components/layout";
 import "../styles/globals.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <title>거울아거울아</title>
+        <meta name="description" content="MirrorMirror" />
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
+
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
-import Head from "next/head";
 import dayjs from "dayjs";
 
 import { Mirror } from "../styles/mirror.styled";
@@ -12,15 +11,9 @@ const Home: NextPage = () => {
   const handleClickMirror = () => push(`/poketmon/${random}`);
 
   return (
-    <>
-      <Head>
-        <title>거울아거울아</title>
-        <meta name="description" content="MirrorMirror" />
-      </Head>
-      <Mirror onClick={handleClickMirror}>
-        <p>거울아 거울아 나는 어떻게 생겼니?</p>
-      </Mirror>
-    </>
+    <Mirror onClick={handleClickMirror}>
+      <p>거울아 거울아 나는 어떻게 생겼니?</p>
+    </Mirror>
   );
 };
 
