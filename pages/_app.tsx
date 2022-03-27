469c2e73740d81a98f38d062b653aec462f6b380
whlee0525 <whlee0525@gmail.com>
2022-03-27 17:38:46 +0900

    add:manifest

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,8 +9,13 @@ export default function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>거울아거울아</title>
-        <meta name="description" content="MirrorMirror" />
+        <meta name="description" content="Poke" />
         <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" href="/icons/heart-152.png" />
+        {/* <link rel="apple-touch-icon" href="/icons/heart-152.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/icons/heart-152.png" />
+        <link rel="apple-touch-icon" sizes="167x167" href="/icons/heart-167.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/heart-180.png" /> */}
       </Head>
 
       <Layout>
